f0f423a3b51b08151728aa46651745a50670db23
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2022-04-22 19:41:48 +0200

    Created repository

---
+++ c1.py
@@ -0,0 +1,26 @@
+# print("|\\")
+# print("| \\")
+# print("|  \\")
+# print("|   \\")
+# print("|____\\")
+
+# print("Ahoj \nMatouši \tVondro")
+
+# a = "Maotuš"
+# print("Ahoj, jmenuji se "+a+10*".")
+
+# a = input("Zadej jméno: ")
+# print("\nJmenuješ se "+a)
+
+# x = input("Zadej první číslo: ")
+# y = input("Zadej druhé číslo: ")
+# z = int(x)+int(y)
+# print("Výsledek je: "+str(z))
+
+a = int(input("Zadej číslo: "))
+if a < 10:
+    print("Číslo je menší jak 10.")
+elif a == 10:
+    print("Číslo je rovno 10")
+else:
+    print("Číslo je větší jak 10.")
